43baa7fd03f2d699db01f450d465526d3cbdd29b
casualdoto <khrestyanovskii@gmail.com>
2025-05-20 23:02:02 +0300

    unused imports

--- src/main/java/com/example/task_manager/repository/jpa/JpaTaskRepository.java
+++ src/main/java/com/example/task_manager/repository/jpa/JpaTaskRepository.java
@@ -4,9 +4,6 @@ import com.example.task_manager.model.Task;
 import com.example.task_manager.repository.TaskRepository;
 import org.springframework.context.annotation.Profile;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
