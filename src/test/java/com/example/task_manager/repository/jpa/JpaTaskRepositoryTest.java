eee13a95b3087ccc7fab3d3f796068bcb465d057
casualdoto <khrestyanovskii@gmail.com>
2025-05-23 05:38:08 +0300

    added MariaDB

--- src/test/java/com/example/task_manager/repository/jpa/JpaTaskRepositoryTest.java
+++ src/test/java/com/example/task_manager/repository/jpa/JpaTaskRepositoryTest.java
@@ -16,7 +16,7 @@ import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
-@ActiveProfiles("prod")
+@ActiveProfiles("test")
 class JpaTaskRepositoryTest {
     
     @Autowired
